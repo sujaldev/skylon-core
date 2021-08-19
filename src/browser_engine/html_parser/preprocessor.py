25209539c3d66d9647e6c5f5c302b8d11100470c
Sujal Singh <email.sujalsingh@gmail.com>
2021-08-19 21:27:10 +0530

    preprocessor partially implemented

---
+++ src/browser_engine/html_parser/preprocessor.py
@@ -0,0 +1,31 @@
+"""
+THIS MODULE REMOVES UNNECESSARY CHARACTERS FROM THE HTML STREAM
+
+THE SPECIFICATION FOR PREPROCESSING HTML IS DEFINED HERE:
+https://html.spec.whatwg.org/multipage/parsing.html#preprocessing-the-input-stream
+"""
+
+NEWLINE = "\u000A"
+CARRIAGE_RETURN = "\u000D"
+
+
+# TODO: IMPLEMENT HTML PREPROCESSOR NON CHAR HANDLING
+def handle_non_char():
+    pass
+
+
+# TODO: IMPLEMENT HTML PREPROCESSOR NON CONTROL CHAR HANDLING
+def handle_control_chars():
+    pass
+
+
+def normalize_newlines(source):
+    normalized = source.replace(CARRIAGE_RETURN + NEWLINE, NEWLINE).replace(CARRIAGE_RETURN, NEWLINE)
+    return normalized
+
+
+def preprocess(stream):
+    handle_non_char()
+    handle_control_chars()
+    processed_stream = normalize_newlines(stream)
+    return processed_stream
