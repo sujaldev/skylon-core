d91ff64721fe9de1962668b35240aee2efaa1488
Sujal Singh <email.sujalsingh@gmail.com>
2021-08-23 18:10:38 +0530

    initialized parser, added dom strucutres

---
+++ src/browser_engine/html_lib/parser.py
@@ -0,0 +1,6 @@
+"""
+THIS IS THE TREE CONSTRUCTION STAGE AS SPECIFIED IN THE SPECIFICATION HERE:
+https://html.spec.whatwg.org/multipage/parsing.html#tree-construction
+"""
+from src.browser_engine.html_lib.tokenizer import HTMLTokenizer
+
